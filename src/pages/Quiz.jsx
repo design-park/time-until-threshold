20aab76851129335369af01b34c2542d193d94cb
design-park <audrey123012@gmail.com>
2025-08-05 16:42:48 +0200

    Updated quiz page

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -23,10 +23,6 @@ function Quiz() {
   const setQ5Answer = useStore((state) => state.setQ5Answer);
   const q5Confidence = useStore((state) => state.q5Confidence);
   const setQ5Confidence = useStore((state) => state.setQ5Confidence);
-  const q6Answer = useStore((state) => state.q6Answer);
-  const setQ6Answer = useStore((state) => state.setQ6Answer);
-  const q6Confidence = useStore((state) => state.q6Confidence);
-  const setQ6Confidence = useStore((state) => state.setQ6Confidence);
 
   useEffect(() => {
     // Scroll to the top of the page when the component mounts
@@ -51,9 +47,6 @@ function Quiz() {
       case "q5":
         setQ5Answer(value);
         break;
-      case "q6":
-        setQ6Answer(value);
-        break;
       default:
         break;
     }
@@ -69,132 +62,123 @@ function Quiz() {
       <section className="survey-section">
         <h1>Quiz</h1>
         <p>
-          The following questions designed to see how much you remember about
-          “the global surface temperature change relative to 1850-1900”, which
-          is the second line graph we showed you during the experiment. The
-          chart illustrated observed (1950-2024) and projected (2025-2099)
-          global surface temperature changes relative to the 1850-1900 baseline.
-          The projections are based on 5 different greenhouse gas emission
-          scenarios:
+          The following questions designed to see how much you{" "}
+          <strong> remember</strong> about
+          <strong>
+            {" "}
+            “the global surface temperature change relative to 1850-1900”
+          </strong>
+          , which is the <strong>second</strong> line graph we showed you during
+          the experiment. The chart illustrated observed (1950-2024) and
+          projected (2025-2099) global surface temperature changes relative to
+          the 1850-1900 baseline. The projections are based on 5 different
+          greenhouse gas emission scenarios:
         </p>
         <ul>
-          <li>SSP1-1.9 (Very low emissions)</li>
-          <li>SSP1-2.6 (Low emissions)</li>
-          <li>SSP2-4.5 (Intermediate emissions)</li>
-          <li>SSP3-7.0 (High emissions)</li>
-          <li>SSP5-8.5 (Very high emissions)</li>
+          <li className="veryLowColor">SSP1-1.9 (Very low emissions)</li>
+          <li className="lowColor">SSP1-2.6 (Low emissions)</li>
+          <li className="intermediateColor">
+            SSP2-4.5 (Intermediate emissions)
+          </li>
+          <li className="highColor">SSP3-7.0 (High emissions)</li>
+          <li className="veryHighColor">SSP5-8.5 (Very high emissions)</li>
         </ul>
         <p>
           The graph also illustrated the years we have left to reach 3
-          thresholds which are: 1.5°C, 2.0°C, and 4.0°C.
+          thresholds which are:<strong> 1.5°C, 2.0°C, and 4.0°C</strong>.
         </p>
         <p>
           Try to answer as best as you can the following questions. If you don’t
-          remember at all, you can choose “I don’t remember”. If you choose the
-          answer, please rate your certainty level as well.
+          remember at all, you can choose “I have no idea”. If you have chosen
+          the answer, please rate your certainty level as well.
         </p>
       </section>
       <form>
-        <section className="survey-section">
-          <h2 className="quiz-question">Question 1</h2>
-          <p>
-            The SSP1-1.9 (Very low emissions) scenario is in what temperature
-            range relative to 1850-1900 at 2099?{" "}
-          </p>
-          <label htmlFor="q1-1">
-            <input
-              id="q1-1"
-              type="radio"
-              name="q1"
-              value="0-1.5°C"
-              checked={q1Answer === "0-1.5°C"}
-              onChange={handleInputChange}
-            />{" "}
-            0-1.5°C
-          </label>{" "}
-          <br />
-          <label htmlFor="q1-2">
-            <input
-              id="q1-2"
-              type="radio"
-              name="q1"
-              value="1.5-2.0°C"
-              checked={q1Answer === "1.5-2.0°C"}
-              onChange={handleInputChange}
-            />{" "}
-            1.5-2.0°C
-          </label>{" "}
-          <br />
-          <label htmlFor="q1-3">
-            <input
-              id="q1-3"
-              type="radio"
-              name="q1"
-              value="2.0-4.0°C"
-              checked={q1Answer === "2.0-4.0°C"}
-              onChange={handleInputChange}
-            />{" "}
-            2.0-4.0°C
-          </label>{" "}
-          <br />
-          <label htmlFor="q1-4">
-            <input
-              id="q1-4"
-              type="radio"
-              name="q1"
-              value="> 4.0°C"
-              checked={q1Answer === "> 4.0°C"}
-              onChange={handleInputChange}
-            />{" "}
-            &gt; 4.0°C
-          </label>{" "}
-          <br />
-          <label htmlFor="q1-5">
-            <input
-              id="q1-5"
-              type="radio"
-              name="q1"
-              value="?"
-              checked={q1Answer === "?"}
-              onChange={handleInputChange}
-            />{" "}
-            I don't know
-          </label>
-          {q1Answer && q1Answer !== "?" && q1Answer !== "" && (
-            <ConfidenceTable questionNumber={1} />
-          )}
-        </section>
+        <NumberQuestion
+          questionNumber={1}
+          question={
+            <>
+              Try to remember in how many years from this year (2025) the{" "}
+              <span className="veryHighColor">
+                {" "}
+                SSP5-8.5 (Very high emissions scenario)
+              </span>{" "}
+              will reach the <strong>1.5°C</strong> threshold. If you're not
+              sure, give the closest estimate you can.
+            </>
+          }
+          textBeforeInput="In"
+          textAfterInput="years"
+        />
+
         <NumberQuestion
           questionNumber={2}
-          question="Try to remember in how many years from this year (2025) the SSP5-8.5 (Very high emissions) scenario will reach the 1.5 °C threshold. It doesn’t need to be precise. Try to remember approximately how many years."
-          textBeforeInput="Approximately in"
+          question={
+            <>
+              Try to remember in how many years from this year (2025) the{" "}
+              <span className="veryHighColor">
+                {" "}
+                SSP5-8.5 (Very high emissions scenario)
+              </span>{" "}
+              will reach the <strong>2.0°C</strong> threshold. If you're not
+              sure, give the closest estimate you can.
+            </>
+          }
+          textBeforeInput="In"
           textAfterInput="years"
         />
 
         <NumberQuestion
           questionNumber={3}
-          question="Try to remember in how many years from this year (2025) the SSP5-8.5 (Very high emissions) scenario will reach the 2.0°C threshold. It doesn’t need to be precise. Try to remember approximately how many years."
-          textBeforeInput="Approximately in"
+          question={
+            <>
+              Try to remember in how many years from this year (2025) the{" "}
+              <span className="veryHighColor">
+                {" "}
+                SSP5-8.5 (Very high emissions scenario)
+              </span>{" "}
+              will reach the <strong>4.0°C</strong> threshold. If you're not
+              sure, give the closest estimate you can.
+            </>
+          }
+          textBeforeInput="In"
           textAfterInput="years"
         />
 
         <NumberQuestion
           questionNumber={4}
-          question="Try to remember in how many years from this year (2025) the SSP5-8.5 (Very high emissions) scenario will reach the 4.0 °C threshold. It doesn’t need to be precise. Try to remember approximately how many years."
-          textBeforeInput="Approximately in"
+          question={
+            <>
+              The graph used{" "}
+              <mark className="yellowBoxColor">a yellow-shaded box</mark> to
+              represent the <strong>6-year gap</strong> between the first and
+              last scenarios that reached the <strong>1.5°C</strong> threshold.
+              Using that as a reference for your memory, what was the year
+              difference between the first and last scenarios that reached the{" "}
+              <strong>2.0°C</strong> threshold, as shown by the width of{" "}
+              <mark className="orangeBoxColor">the orange-shaded box</mark>? If
+              you're unsure, give your best estimate.
+            </>
+          }
+          textBeforeInput=""
           textAfterInput="years"
         />
 
         <NumberQuestion
           questionNumber={5}
-          question="The year difference between the first and last scenario that reached the 1.5°C threshold was 6 years. Then what was the year different between the first and last scenario that reached the 2.0°C threshold?"
-          textBeforeInput="Approximately in"
-          textAfterInput="years"
-        />
-
-        <NumberQuestion
-          questionNumber={6}
-          question="The year difference between the first and last scenario that reached the 1.5°C threshold was 6 years. Then what was the year difference between the first and last scenario that reached the 4.0°C threshold?"
+          question={
+            <>
+              The graph used{" "}
+              <mark className="yellowBoxColor">a yellow-shaded box</mark> to
+              represent the <strong>6-year gap</strong> between the first and
+              last scenarios that reached the <strong>1.5°C</strong> threshold.
+              Using that as a reference for your memory, what was the year
+              difference between the first and last scenarios that reached the{" "}
+              <strong>4.0°C</strong> threshold, as shown by the width of{" "}
+              <mark className="redBoxColor">the red-shaded box</mark>? If
+              you're unsure, give your best estimate.
+            </>
+          }
           textBeforeInput="More than"
           textAfterInput="years"
         />
@@ -246,7 +230,7 @@ function NumberQuestion({
           checked={dontKnow}
           onChange={handleDontKnowChange}
         />{" "}
-        I don't know
+        I have no idea
       </label>
       {value !== "" && <ConfidenceTable questionNumber={questionNumber} />}
     </section>
@@ -259,7 +243,6 @@ function ConfidenceTable({ questionNumber }) {
   const q3Confidence = useStore((state) => state.q3Confidence);
   const q4Confidence = useStore((state) => state.q4Confidence);
   const q5Confidence = useStore((state) => state.q5Confidence);
-  const q6Confidence = useStore((state) => state.q6Confidence);
   const confidence = useMemo(() => {
     switch (questionNumber) {
       case 1:
@@ -272,8 +255,6 @@ function ConfidenceTable({ questionNumber }) {
         return q4Confidence;
       case 5:
         return q5Confidence;
-      case 6:
-        return q6Confidence;
       default:
         return null;
     }
@@ -284,14 +265,12 @@ function ConfidenceTable({ questionNumber }) {
     q3Confidence,
     q4Confidence,
     q5Confidence,
-    q6Confidence,
   ]);
   const setQ1Confidence = useStore((state) => state.setQ1Confidence);
   const setQ2Confidence = useStore((state) => state.setQ2Confidence);
   const setQ3Confidence = useStore((state) => state.setQ3Confidence);
   const setQ4Confidence = useStore((state) => state.setQ4Confidence);
   const setQ5Confidence = useStore((state) => state.setQ5Confidence);
-  const setQ6Confidence = useStore((state) => state.setQ6Confidence);
   const handleConfidenceChange = (e) => {
     const { value } = e.target;
     switch (questionNumber) {
@@ -309,9 +288,6 @@ function ConfidenceTable({ questionNumber }) {
         break;
       case 5:
         setQ5Confidence(value);
-        break;
-      case 6:
-        setQ6Confidence(value);
         break;
       default:
         break;
