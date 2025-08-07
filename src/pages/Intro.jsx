2b8e4736ba030a6a0d6fc7b30511ac8a06f61d69
design-park <audrey123012@gmail.com>
2025-08-07 18:52:25 +0200

    changed consent form

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -34,69 +34,11 @@ function Intro() {
       <div className="main-container">
         <div className="card">
           <h4 className="instruction-text">
-            A study on the impact of climate change visualization
+            Please read the following information about my research project and
+            sign the consent form to proceed.
           </h4>
-
-          <p className="subText">
-            You are invited to participate in our study called
-            <strong>
-              {" "}
-              "A study on the impact of climate change visualization."
-            </strong>
-          </p>
-
-          <p className="subText">This study will take around 30 minutes.</p>
-
-          <p className="subText">
-            Our goal is to understand how interactive data visualizations can
-            support public understanding and involvement in climate change
-            issues. The results from this experiment will help inform the design
-            of visualizations that more effectively raise awareness and
-            encourage meaningful engagement.
-          </p>
-
-          <p className="subText">
-            Please participate in this experiment only if you meet all of the
-            following criteria:
-          </p>
-          <ul className="subText">
-            <li>You are fluent in English</li>
-            <li>You are at least 18 years old</li>
-            <li>
-              You do not have <strong>low vision</strong> or{" "}
-              <strong>any form of color vision deficiency</strong>
-            </li>
-            <li>You are completing this survey on a computer screen</li>
-          </ul>
-
-          <p className="subText">
-            <strong>
-              You can only participate using a computer screen, as a mobile
-              phone's screen is too small to display the visualizations
-              correctly.
-            </strong>
-          </p>
-
-          <p className="subText">
-            After agreeing to the terms (on the next page), you will begin the
-            experiment. You will be seated at a computer with a screen and
-            keyboard, and you will follow the instructions displayed on the
-            screen. The tasks will include answering questionnaires, reading
-            content, interacting with visualizations, and solving quizzes. Most
-            activities will be completed on the computer; however, at certain
-            points, the researcher may provide you with paper materials and a
-            pen, which will require you to read or write by hand. The researcher
-            will be present throughout the process, and you are welcome to ask
-            questions at any time. Your responses during the study will be
-            recorded.
-          </p>
-
-          <p className="subText">
-            This study is conducted by Jihyun Park (Intern researcher, ILDA
-            Team, Inria Saclay) under the supervision of Florent Cabric and
-            Vanessa Peña Araya. If you have any questions or concerns about the
-            study, please feel free to contact Jihyun Park at{" "}
-          </p>
+          <h4 className="introTitle">Title of the project:</h4>
+          <p className="introParagraph">Time Until Threshold</p>
 
           <h4 className="titleText">Participant Code:</h4>
           <p className="subText">{participantCode}</p>
@@ -118,30 +60,33 @@ function Intro() {
             )
           </p>
 
-          <h4 className="titleText">Purpose of the research project:</h4>
-
-          <p className="subText">
-            Version 2: The purpose of this research is to investigate how
-            different visual representations of climate change data influence
-            public understanding and engagement with the issue.
+          <h4 className="introTitle">Purpose of the research project:</h4>
+          <p className="introParagraph">
+            Our goal is to understand how interactive data visualizations can
+            support public understanding and involvement in climate change
+            issues. The results from this experiment will help inform the design
+            of visualizations that more effectively raise awareness and
+            encourage meaningful engagement.
           </p>
 
-          <h4 className="titleText">What is expected from you:</h4>
-          <p className="subText">
-            The experiment is expected to last approximately 25 minutes. You
-            will be seated at a computer with a screen and keyboard. You can
-            proceed with what the computer prompts you to do. It will include
-            answering questionnaires, reading, and interacting with the
-            visualization. For the reading, you would be provided with the
-            material on paper. When prompted by the computer, please ask the
-            researcher for the paper material. Your responses throughout the
-            study will be recorded.
+          <h4 className="introTitle">What is expected from you:</h4>
+          <p className="introParagraph">
+            After agreeing to the terms, you will begin the experiment. You will
+            be seated at a computer with a screen and keyboard, and you will
+            follow the instructions displayed on the screen. The tasks will
+            include answering questionnaires, reading content, interacting with
+            visualizations, and solving quizzes. Most activities will be
+            completed on the computer; however, at certain points, the
+            researcher may provide you with paper materials and a pen, which
+            will require you to read or write by hand. The researcher will be
+            present throughout the process, and you are welcome to ask questions
+            at any time. Your responses during the study will be recorded.
           </p>
 
-          <h4 className="titleText">
+          <h4 className="introTitle">
             Your right to withdraw from the research at any time:
           </h4>
-          <p className="subText">
+          <p className="introParagraph">
             Your participation in this research is voluntary, and you can stop
             at any time without providing any justification. Your decision to
             participate, refuse to participate, or stop participating will not
@@ -149,10 +94,10 @@ function Intro() {
             entitled.
           </p>
 
-          <h4 className="titleText">
+          <h4 className="introTitle">
             Your rights to confidentiality and privacy:
           </h4>
-          <p className="subText">
+          <p className="introParagraph">
             The data obtained will be treated with the utmost confidentiality.
             Your identity will be masked using a random number (participant
             code), and no other information will reveal it. Only the principal
@@ -161,44 +106,43 @@ function Intro() {
             the consent form.
           </p>
 
-          <h4 className="titleText">Benefits of the study:</h4>
-          <p className="subText">
+          <h4 className="introTitle">Benefits of the study:</h4>
+          <p className="introParagraph">
             You may become more informed about the issue of climate change and
             its potential impacts.
           </p>
 
-          <h4 className="titleText">Possible risks of the study:</h4>
-          <p className="subText">
+          <h4 className="introTitle">Possible risks of the study:</h4>
+          <p className="introParagraph">
             While there are no anticipated physical risks, some participants may
             experience temporary feelings of anxiety or concern when engaging
             with information about climate change projections.
           </p>
 
-          <h4 className="titleText">Dissemination:</h4>
-          <p className="subText">
+          <h4 className="introTitle">Dissemination:</h4>
+          <p className="introParagraph">
             The results of this research will be used for academic purposes,
             primarily for the completion of a master's thesis.
           </p>
 
-          <h4 className="titleText">Your right to ask questions:</h4>
-          <p className="subText">
+          <h4 className="introTitle">Your right to ask questions:</h4>
+          <p className="introParagraph">
             You may ask questions about the research at any time(before, during,
             and after your participation) by contacting the project's principal
-            investigator.(
-            <a href="mailto:jihyun.park@inria.fr">jihyun.park@inria.fr</a>).
+            investigator(jihyun.park@inria.fr).
           </p>
 
-          <h4 className="titleText">Consent to paticipate:</h4>
-          <p className="subText">
-            You can now sign the consent form given to you. By doing it, you
-            certify that you have read and understood the information above,
+          <h4 className="introTitle">Consent to paticipate:</h4>
+          <p className="introParagraph">
+            By ticking the checkbox and clicking the "Continue" button below,
+            you certify that you have read and understood the information above,
             that the researcher has answered your questions satisfactorily, and
             that you have been advised that you are free to withdraw your
             consent or withdraw from this research at any time.
           </p>
 
           <label className="checkBox">
-            I have signed the consent form:
+            I consent to participate in this research project:
             <input
               type="checkbox"
               name="consentCheckbox"
